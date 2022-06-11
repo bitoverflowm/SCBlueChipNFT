02a9e3fd55eb84d9e067bea74d21189b71118f74
EduardoCL <35414231+EduardoCortesL@users.noreply.github.com>
2022-06-10 18:33:02 -0700

    added up to date smart contract

--- contracts/v1.sol
+++ contracts/v1.sol
@@ -1,29 +1,28 @@
 // SPDX-License-Identifier: MIT
 
-//Chaning token URI to always be the same 
-//Creating function to show total supply 
-//Address can mint multiple times 
+//Version C subversion 5
+//Chaning token URI to always be the same <--- need to check this 
 //Whitelist overrides if set multiple times so only 1 transaction with all addres 
+//Crossmint working fully  
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Fonzi01 is ERC721, ERC721Enumerable, Ownable {
-    //string public PROVENANCE;
+contract FonzV1c5 is ERC721, ERC721Enumerable, Ownable {
     bool public saleIsActive = false;
     //string private _baseURIextended;
 
     bool public isAllowListActive = false;
-    uint256 public constant MAX_SUPPLY = 50000;
+    uint256 public constant MAX_SUPPLY = 5000;
     uint256 public constant MAX_PUBLIC_MINT = 2;
-    uint256 public constant PRICE_PER_TOKEN = 1 ether;
+    uint256 public constant PRICE_PER_TOKEN = 100000000000000000; //in Wei
 
     mapping(address => uint8) private _allowList;
 
-    constructor() ERC721("FONZI-TEST-V1", "FONZV1") {
+    constructor() ERC721("FTV1c5", "FV1c5") {
     }
 
     function setIsAllowListActive(bool _isAllowListActive) external onlyOwner {
@@ -66,28 +65,34 @@ contract Fonzi01 is ERC721, ERC721Enumerable, Ownable {
        function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        return "ipfs://Qmck3GpzP8uhXgpkUUKnHMH3DZKUpwf1U9BgyvLKFNK6r6"; // 1 ?
+        return "ipfs://QmekvbsCsnTYsNhmCohiih8oNFNkw7vyFVGwWz1iaN835R"; //need to change to actual nft 
     }
-    /* OLD Base URI
-    function setBaseURI(string memory baseURI_) external onlyOwner() {
-        _baseURIextended = baseURI_;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseURIextended;
-    }
-    */
-
+    
     function reserve(uint256 n) public onlyOwner {
-      uint supply = totalSupply();
+      uint ts = totalSupply();
+      require(ts + n <= MAX_SUPPLY, "Purchase would exceed max possible tokens.");
       uint i;
       for (i = 0; i < n; i++) {
-          _safeMint(msg.sender, supply + i);
+          _safeMint(msg.sender, ts + i);
       }
     }
 
     function setSaleState(bool newState) public onlyOwner {
         saleIsActive = newState;
+    }
+
+    function creditCardMint(address to, uint numberOfTokens) external payable {
+        uint256 ts = totalSupply();
+        address crossmint_eth = 0xdAb1a1854214684acE522439684a145E62505233;
+        require(ts + numberOfTokens <= MAX_SUPPLY, "Purchase would exceed max possible tokens. You can get a BCNFT on secondary market!");
+        require ( msg.sender == crossmint_eth, "Only crosssmint address can call this function");
+        require(saleIsActive, "Sale must be active to mint tokens");
+        require(PRICE_PER_TOKEN * numberOfTokens <= msg.value, "Ether value sent is not correct");
+
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(to, ts + i);
+        }
+       
     }
 
     function mint(uint numberOfTokens) public payable {
